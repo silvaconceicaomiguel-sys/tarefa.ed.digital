8e50436ea0bdffce87c1ce6df474f244d386edf2
silvaconceicaomiguel-sys <silva.conceicao.miguel@escola.pr.gov.br>
2026-09-16 13:43:22 -0300

    a

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const botoesCurtir = document.querySelectorALL(".curtir");
+botoesCurtir.forEach(function(botaoCurti){
 
+let curtir = false;
+botaoCurtir.addEventListener("click", curtir);
+function curtir(){
+const contador = botaoCurtir.querySelector("spen");
+
+
+
+}
+
+
+
+
+}
